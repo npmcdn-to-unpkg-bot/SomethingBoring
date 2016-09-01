08206a0f5c439bee0dd8f10ecf9912e3ab737786
hezedu <hezedu@hotmail.com>
2016-09-02 01:02:57 +0800

    home

--- webpack_test/src/cmpt/common.jsx
+++ webpack_test/src/cmpt/common.jsx
@@ -5,28 +5,32 @@ import style from '../css/style.scss';
 //import getMuiTheme from 'material-ui/styles/getMuiTheme'; , PropTypes
 //import {darkWhite, lightWhite, grey900} from 'material-ui/styles/colors';
 //import AppBar from 'material-ui/AppBar';
-
-
 export class Root extends Component {
-  // static propTypes = {
-  //   children: PropTypes.node,
-  //   location: PropTypes.object,
-  //   muiTheme: getMuiTheme()
-  // };
-  // componentWillMount() {
-  //   this.setState({
-  //     muiTheme: getMuiTheme(),
-  //   });
-  // }
-
   render() {
     return (
-      <div id='root'>
-       {/*<AppBar title='title' />*/}
+      <div className='height100'>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className={style.homeTitle}>Home</div>
+    );
+  }
+}
+
+export class Master extends Component {
+  render() {
+    return (
+      <div className='height100'>
         <div className={style.topNavWarp}>
           <Link className={style.title} to="/">rtmap前端架构</Link>
           <div className={style.topNav}>
-            <Link to="env" activeClassName={style.active}>环境</Link>
+            <Link to="/env" activeClassName={style.active}>环境</Link>
             <Link to="/React">React</Link>
             <Link to="/cmpt">组件库</Link>
           </div>
@@ -35,13 +39,6 @@ export class Root extends Component {
           {this.props.children}
         </div>
       </div>
-    );
-  }
-}
-export class Home extends Component {
-  render() {
-    return (
-      <div>Home</div>
     );
   }
 }
--- webpack_test/src/router.jsx
+++ webpack_test/src/router.jsx
@@ -2,9 +2,11 @@ import * as cmpt from './cmpt/common.jsx';
 import { Route, IndexRoute} from 'react-router';
 export default (
   <Route path="/" component={cmpt.Root}>
-    <IndexRoute component={cmpt.Home}/>
-    <Route path="env" component={cmpt.Bottom}>
-      <Route path="about" component={cmpt.About}/>
+    <Route component={cmpt.Master}>
+      <IndexRoute component={cmpt.Home}/>
+      <Route path="env" component={cmpt.Bottom}>
+        <Route path="about" component={cmpt.About}/>
+      </Route>
     </Route>
   </Route>
 );
